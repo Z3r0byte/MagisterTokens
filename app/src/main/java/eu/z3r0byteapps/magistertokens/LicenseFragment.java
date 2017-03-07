6f63971295be69c49b320725f464fdb587edc2d7
Z3r0byte <basvandenboom3@gmail.com>
2017-03-07 19:05:26 +0100

    added working on license purchases

--- app/src/main/java/eu/z3r0byteapps/magistertokens/LicenseFragment.java
+++ app/src/main/java/eu/z3r0byteapps/magistertokens/LicenseFragment.java
@@ -1,12 +1,19 @@
 package eu.z3r0byteapps.magistertokens;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import com.android.vending.billing.IInAppBillingService;
 
 import java.util.Date;
 
@@ -17,9 +24,28 @@ import eu.z3r0byteapps.magistertokens.Util.DateUtils;
 public class LicenseFragment extends Fragment {
     View view;
 
+    TextView buyLicense;
+    TextView refreshLicense;
+    TextView infoLicense;
+
     public LicenseFragment() {
         // Required empty public constructor
     }
+
+    IInAppBillingService mService;
+
+    ServiceConnection mServiceConn = new ServiceConnection() {
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            mService = null;
+        }
+
+        @Override
+        public void onServiceConnected(ComponentName name,
+                                       IBinder service) {
+            mService = IInAppBillingService.Stub.asInterface(service);
+        }
+    };
 
 
     @Override
@@ -46,7 +72,25 @@ public class LicenseFragment extends Fragment {
             status.setText(R.string.msg_premium);
         }
 
+        buyLicense = (TextView) view.findViewById(R.id.buy_license);
+        refreshLicense = (TextView) view.findViewById(R.id.refresh_license);
+        infoLicense = (TextView) view.findViewById(R.id.info_licenses);
+
+
+        Intent serviceIntent =
+                new Intent("com.android.vending.billing.InAppBillingService.BIND");
+        serviceIntent.setPackage("com.android.vending");
+        getActivity().bindService(serviceIntent, mServiceConn, Context.BIND_AUTO_CREATE);
+
 
         return view;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mService != null) {
+            getActivity().unbindService(mServiceConn);
+        }
     }
 }
